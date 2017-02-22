00ce6b7b0e8812a0a74953358bbbe3fa4e660d6e
mukesh093 <muksh093@gmail.com>
2017-02-22 15:50:44 +0530

    VideoCategoryController class added

--- APPYSTORE/VideoCategoryController.swift
+++ APPYSTORE/VideoCategoryController.swift
@@ -13,6 +13,7 @@ class VideoCategoryController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
     }
 
     override func didReceiveMemoryWarning() {
