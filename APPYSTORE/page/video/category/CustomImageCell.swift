e6fd140800b410cd45b72a3bf0a4c10d9214bfb8
mukesh093 <muksh093@gmail.com>
2017-02-24 18:50:55 +0530

    UIView added

---
+++ APPYSTORE/page/video/category/CustomImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomImageCell.swift
+//  APPYSTORE
+//
+//  Created by BridgeLabz on 24/02/17.
+//  Copyright © 2017 MAUJ MOBILE PVT LTD. All rights reserved.
+//
+
+import UIKit
+
+class CustomImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var bgImage: UIImageView!
+    @IBOutlet weak var logoBtn: UIButton!
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var label: UILabel!
+   // @IBOutlet weak var bgLabel: UILabel!
+}
--- APPYSTORE/page/video/category/VideoCategoryController.swift
+++ APPYSTORE/page/video/category/VideoCategoryController.swift
@@ -7,15 +7,58 @@
 //
 
 import UIKit
+//import Foundation
+//import AVFoundation
 
-class VideoCategoryController: BaseViewController {
+//private var playerViewControllerKVOContext = 0
 
+class VideoCategoryController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    //MARK: variables
+    let image = ["ic_launcher", "ic_launcher", "ic_launcher", "ic_launcher", "ic_launcher", "ic_launcher"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return image.count
     }
-}
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as!  CustomImageCell
+        
+        cell.bgImage.layer.cornerRadius = 64
+        cell.bgImage.clipsToBounds = true
+        
+        cell.logoBtn.layer.cornerRadius = 25
+        cell.logoBtn.clipsToBounds = true
+        
+      //  cell.bgLabel.layer.cornerRadius = 64
+      //  cell.bgLabel.clipsToBounds = true
+        
+        cell.mainView.layer.cornerRadius = 64
+        cell.mainView.clipsToBounds = true
+        
+        cell.mainView.layer.shadowColor = UIColor.black.cgColor
+        cell.mainView.layer.shadowOpacity = 1
+        cell.mainView.layer.shadowOffset = CGSize.zero
+        cell.mainView.layer.shadowRadius = 10
+        
+        cell.mainView.layer.shadowPath = UIBezierPath(rect: cell.mainView.bounds).cgPath
+        cell.mainView.layer.shouldRasterize = true
+
+            
+        cell.bgImage.image = UIImage(named: image[indexPath.row])
+        return cell
+    }
+    
+    //width = 48
+    // height= 32
+   
+ }
 
