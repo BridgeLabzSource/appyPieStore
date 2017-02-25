8e96c572908d58317f435d6399eda122133131cf
rahulmandalkar <rahulmandalkar431@gmail.com>
2017-02-25 10:25:44 +0530

    added history page

---
+++ APPYSTORE/page/HistoryController.swift
@@ -0,0 +1,21 @@
+
+//  HistoryController.swift
+//  APPYSTORE
+//
+//  Created by BridgeLabz Solution LLP on 25/02/17.
+//  Copyright © 2017 MAUJ MOBILE PVT LTD. All rights reserved.
+
+
+import UIKit
+
+class HistoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+   
+
+}
