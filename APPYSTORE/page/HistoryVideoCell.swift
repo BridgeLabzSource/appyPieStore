931cd7a872828552be399224a41c52e60e6e59f1
rahulmandalkar <rahulmandalkar431@gmail.com>
2017-02-27 09:59:07 +0530

    alamofire

---
+++ APPYSTORE/page/HistoryVideoCell.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryVideoCell.swift
+//  APPYSTORE
+//
+//  Created by BridgeLabz Solution LLP on 25/02/17.
+//  Copyright © 2017 MAUJ MOBILE PVT LTD. All rights reserved.
+//
+
+import UIKit
+
+class HistoryVideoCell: UICollectionViewCell {
+    
+}
