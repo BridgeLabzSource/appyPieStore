2f817e351eb3303a4693d66bcaf379c97231943b
mukesh093 <muksh093@gmail.com>
2017-03-08 17:49:11 +0530

    added UILabelExtension

---
+++ APPYSTORE/common/UILabelExtension.swift
@@ -0,0 +1,22 @@
+//
+//  CustomFontStyle.swift
+//  APPYSTORE
+//
+//  Created by BridgeLabz on 08/03/17.
+//  Copyright © 2017 MAUJ MOBILE PVT LTD. All rights reserved.
+//
+
+import UIKit
+
+    extension UILabel {
+        
+        var defaultFontStyle : String {
+            get {
+                return self.font.fontName
+            }
+            
+            set {
+                self.font = UIFont(name: newValue, size: self.font.pointSize)
+            }
+        }
+    }
