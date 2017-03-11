a58e3d58952056867d037478a7bf89b17d7a24d0
mukesh093 <muksh093@gmail.com>
2017-03-11 12:06:29 +0530

    changed viewController

--- APPYSTORE/common/BottomBarOverlay.swift
+++ APPYSTORE/common/BottomBarOverlay.swift
@@ -11,26 +11,62 @@ import UIKit
 @IBDesignable class BottomBarOverlay: UIView {
 
     @IBOutlet var rootView: UIView!
-    func initialize()
-    {
-        UINib(nibName: "BottomBarOverlay", bundle: nil).instantiate(withOwner: self, options: nil)
-        
-        addSubview(rootView)
-        //rootView.bindFrameToSuperviewBounds()
-        rootView.frame = self.bounds
-        
-    }
+    
+//    func initialize()
+//    {
+//        UINib(nibName: "BottomBarOverlay", bundle: nil).instantiate(withOwner: self, options: nil)
+//        
+//        addSubview(rootView)
+//        //rootView.bindFrameToSuperviewBounds()
+//        rootView.frame = self.bounds
+//        
+//    }
+//    
+//    override init(frame: CGRect)
+//    {
+//        super.init(frame: frame)
+//        initialize()
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder)
+//    {
+//        super.init(coder: aDecoder)
+//        initialize()
+//    }
+    
+    
+    //MARK: Declarations
+    var view: UIView!
     
     override init(frame: CGRect)
     {
         super.init(frame: frame)
-        initialize()
+        setup()
     }
     
     required init?(coder aDecoder: NSCoder)
     {
         super.init(coder: aDecoder)
-        initialize()
+        setup()
     }
+    
+    func setup()
+    {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = UIViewAutoresizing.flexibleWidth;
+        view.autoresizingMask = UIViewAutoresizing.flexibleHeight
+        addSubview(view)
+        
+    }
+    
+    func loadViewFromNib() -> UIView
+    {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "BottomBarOverlay", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        return view
+    }
+
 
 }
