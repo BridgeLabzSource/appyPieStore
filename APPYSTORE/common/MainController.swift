a10cbf4ed7c7d08067b942c483a13a9c3ab4e940
ios dev <ios_dev@maujs-macbook-pro-2.local>
2017-03-29 16:36:45 +0530

    base controller rename

--- APPYSTORE/common/MainController.swift
+++ APPYSTORE/common/MainController.swift
@@ -1,7 +1,7 @@
 
 import UIKit
 
-class BaseController: UIViewController {
+class MainController: UIViewController {
     
     @IBOutlet var topView: TopBarOverlay!
     @IBOutlet var middleView: UIView!
@@ -9,21 +9,21 @@ class BaseController: UIViewController {
     
     weak var currentViewController: UIViewController!
     
-    lazy var historycontroller: HistoryController = {
+    lazy var historyController: HistoryController = {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "HistoryController") as! HistoryController
         
-        self.addAsChildViewController(childView:viewController)
+        self.addAsChildViewController(childView: viewController)
         
         return viewController
     }()
     
     
-    lazy var videocontroller:VideoCategoryController = {
+    lazy var videoController: VideoCategoryController = {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "VideoCategoryController") as! VideoCategoryController
         
-        self.addAsChildViewController(childView:viewController)
+        self.addAsChildViewController(childView: viewController)
         
         return viewController
     }()
@@ -31,33 +31,30 @@ class BaseController: UIViewController {
     override func viewDidLoad() {
         topView.translatesAutoresizingMaskIntoConstraints = false
         middleView.translatesAutoresizingMaskIntoConstraints = false
-        
         bottomView.translatesAutoresizingMaskIntoConstraints = false
         
-        topView.videoButton.addTarget(self, action: #selector(video), for: .touchUpInside)
-        topView.historyButton.addTarget(self, action: #selector(history), for: .touchUpInside)
+        topView.videoButton.addTarget(self, action: #selector(showVideoCategoryPage), for: .touchUpInside)
+        topView.historyButton.addTarget(self, action: #selector(showHistoryPage), for: .touchUpInside)
         super.viewDidLoad()
         
         
     }
     
-    func video() {
-        print("Video Page")
+    func showVideoCategoryPage() {
         removeFromParentViewController()
-        historycontroller.view.alpha = 0
-        videocontroller.view.alpha = 1
-        videocontroller.view.isHidden = false
-        self.view.bringSubview(toFront: historycontroller.view)
+        historyController.view.alpha = 0
+        videoController.view.alpha = 1
+        videoController.view.isHidden = false
+        self.view.bringSubview(toFront: historyController.view)
         
     }
     
-    func history() {
-        print("History Page")
+    func showHistoryPage() {
         removeFromParentViewController()
-        videocontroller.view.alpha = 0
-        historycontroller.view.alpha = 1
-        historycontroller.view.isHidden = false
-        self.view.bringSubview(toFront: historycontroller.view)
+        videoController.view.alpha = 0
+        historyController.view.alpha = 1
+        historyController.view.isHidden = false
+        self.view.bringSubview(toFront: historyController.view)
     }
     
     private func addAsChildViewController(childView:UIViewController){
@@ -65,7 +62,7 @@ class BaseController: UIViewController {
         view.addSubview(childView.view)
         childView.view.backgroundColor = UIColor.clear
         childView.view.frame = middleView.frame
-        print(self.middleView.frame.origin.y)
+        //print(self.middleView.frame.origin.y)
         childView.didMove(toParentViewController: childView)
     }
     
---
+++ APPYSTORE/page/video/VideoListingController.swift
@@ -0,0 +1,17 @@
+//
+//  VideoListingController.swift
+//  APPYSTORE
+//
+//  Created by ios_dev on 28/03/17.
+//  Copyright © 2017 MAUJ MOBILE PVT LTD. All rights reserved.
+//
+
+import UIKit
+
+class VideoListingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
