d7fc06574aa305c1121aa6ea8a564b144026de26
ios_dev <ios_dev@Maujs-MacBook-Pro-2.local>
2017-04-12 12:55:44 +0530

    added two files

---
+++ APPYSTORE/common/BaseViewControllerProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  BaseViewControllerProtocol.swift
+//  APPYSTORE
+//
+//  Created by ios_dev on 12/04/17.
+//  Copyright © 2017 MAUJ MOBILE PVT LTD. All rights reserved.
+//
+
+import Foundation
