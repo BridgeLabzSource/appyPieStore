e21826c5d50ee181579664764251f350a2d021ef
ios_dev <ios_dev@Maujs-MacBook-Pro-2.local>
2017-04-15 22:48:57 +0530

    search completed

---
+++ APPYSTORE/common/TagsCard.swift
@@ -0,0 +1,18 @@
+//
+//  TagsCard.swift
+//  APPYSTORE
+//
+//  Created by ios_dev on 13/04/17.
+//  Copyright © 2017 MAUJ MOBILE PVT LTD. All rights reserved.
+//
+
+import UIKit
+
+class TagsCard: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ APPYSTORE/page/SearchTagsParser.swift
@@ -0,0 +1,9 @@
+//
+//  SearchTagsParser.swift
+//  APPYSTORE
+//
+//  Created by ios_dev on 13/04/17.
+//  Copyright © 2017 MAUJ MOBILE PVT LTD. All rights reserved.
+//
+
+import Foundation
